fddf9ccc3535b6d4d79afddd20d0c6c241f69ea4
@john94099 <ttnhan1@cmcglobal.vn>
2022-11-08 13:20:27 +0700

    Add project files.

---
+++ RedisConnectorHelper.cs
@@ -0,0 +1,25 @@
+﻿using StackExchange.Redis;
+
+namespace LocalCaching
+{
+    public class RedisConnectorHelper
+    {
+        static RedisConnectorHelper()
+        {
+            RedisConnectorHelper.lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+            {
+                return ConnectionMultiplexer.Connect("localhost");
+            });
+        }
+
+        private static Lazy<ConnectionMultiplexer> lazyConnection;
+
+        public static ConnectionMultiplexer Connection
+        {
+            get
+            {
+                return lazyConnection.Value;
+            }
+        }
+    }
+}
