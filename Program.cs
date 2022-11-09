056ffdfb250a3e5c4703de969403c22874481854
@john94099 <ttnhan1@cmcglobal.vn>
2022-11-09 10:04:49 +0700

    demo delete

--- Program.cs
+++ Program.cs
@@ -4,17 +4,32 @@ class Program
     static void Main(string[] args)
     {
         var program = new Program();
+
         Console.WriteLine("Saving random data in cache");
         program.SaveBigData();
         Console.WriteLine("Reading data from cache");
         program.ReadData();
+
+        //Console.WriteLine("remove data from cache");
+        //program.RemoveAllData();
         Console.ReadLine();
+    }
+
+    private void RemoveAllData()
+    {
+        var cache = RedisConnectorHelper.Connection.GetDatabase(15);
+        var devicesCount = 10;
+        for (int i = 0; i < devicesCount; i++)
+        {
+            var value = cache.KeyDelete($"Device_Status:{i}");
+            Console.WriteLine($"Valor={value}");
+        }
     }
 
     public void ReadData()
     {
-        var cache = RedisConnectorHelper.Connection.GetDatabase();
-        var devicesCount = 10000;
+        var cache = RedisConnectorHelper.Connection.GetDatabase(15);
+        var devicesCount = 10;
         for (int i = 0; i < devicesCount; i++)
         {
             var value = cache.StringGet($"Device_Status:{i}");
@@ -24,12 +39,12 @@ class Program
 
     public void SaveBigData()
     {
-        var devicesCount = 10000;
+        var devicesCount = 10;
         var rnd = new Random();
-        var cache = RedisConnectorHelper.Connection.GetDatabase();
+        var cache = RedisConnectorHelper.Connection.GetDatabase(15);
         for (int i = 1; i < devicesCount; i++)
         {
-            var value = rnd.Next(0, 10000);
+            var value = rnd.Next(0, 10);
             cache.StringSet($"Device_Status:{i}", value);
         }
     }
